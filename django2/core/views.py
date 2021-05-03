5934c76845beaee0d6359566135bfb645d47f30c
HenriqueMAlves <m.henriquealves@gmail.com>
2021-05-02 21:12:18 -0300

    feat: :art: Definir seção administrativa
    Somente é possível criar um produto caso o usário esteja conectado a uma conta

--- django2/core/views.py
+++ django2/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 from .forms import ContatoForm, ProdutoModelForm
@@ -30,19 +30,22 @@ def contato(request):
 
 
 def produto(request):
-    if str(request.method) == 'POST':
-        form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
 
-            form.save()
+                form.save()
 
-            messages.success(request, 'Produto salvo com sucesso.')
-            form = ProdutoModelForm()
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto')
         else:
-            messages.error(request, 'Erro ao salvar produto')
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
     else:
-        form = ProdutoModelForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'produto.html', context)
+        return redirect('index')
